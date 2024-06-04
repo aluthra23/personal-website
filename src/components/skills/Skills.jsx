7b0d3df76a1381df35eb6f6196a3308998696e0c
Arav Luthra <aravluthra@aravs-air.lan>
2024-06-04 19:51:17 -0400

    addd

--- src/components/skills/Skills.jsx
+++ src/components/skills/Skills.jsx
@@ -27,25 +27,55 @@ const Skills = () => {
                 title="Python"
                 description="Experienced in Python programming language with libraries such as NumPy, Pandas, Matplotlib, Seaborn, Beautiful Soup, and csv."
                 level="Advanced"
-                logo="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1869px-Python-logo-notext.svg.png" // Replace with actual logo link
+                logo="https://upload.wikimedia.org/wikipedia/commons/thumb/c/c3/Python-logo-notext.svg/1869px-Python-logo-notext.svg.png"
               />
               <SkillCard
                 title="Java"
                 description="Proficient in Java programming language and Object-Oriented Programming."
                 level="Advanced"
-                logo="https://logos-download.com/wp-content/uploads/2016/10/Java_logo_icon.png" // Replace with actual logo link
+                logo="https://logos-download.com/wp-content/uploads/2016/10/Java_logo_icon.png"
               />
               <SkillCard
                 title="R"
                 description="Experienced in R for statistical computing and data visualization using libraries like ggplot2 and dplyr."
                 level="Intermediate"
-                logo="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/R_logo.svg/724px-R_logo.svg.png" // Replace with actual logo link
+                logo="https://upload.wikimedia.org/wikipedia/commons/thumb/1/1b/R_logo.svg/724px-R_logo.svg.png"
               />
               <SkillCard
                 title="Flutter"
                 description="Proficient in developing cross-platform mobile applications using Flutter framework."
                 level="Intermediate"
                 logo="https://storage.googleapis.com/cms-storage-bucket/0dbfcc7a59cd1cf16282.png"
+              />
+              <SkillCard
+                title="API Integration"
+                description="Experienced in integrating APIs for web and mobile applications, including RESTful APIs (GitHub) and third-party services."
+                level="Learning"
+                logo="https://cdn-icons-png.flaticon.com/256/2164/2164832.png"
+              />
+              <SkillCard
+                title="Dart"
+                description="Proficient in Dart programming language for developing mobile and web applications."
+                level="Intermediate"
+                logo="https://upload.wikimedia.org/wikipedia/commons/7/7e/Dart-logo.png"
+              />
+              <SkillCard
+                title="HTML"
+                description="Proficient in HTML for building web pages and web applications."
+                level="Intermediate"
+                logo="https://upload.wikimedia.org/wikipedia/commons/6/61/HTML5_logo_and_wordmark.svg"
+              />
+              <SkillCard
+                title="CSS"
+                description="Proficient in CSS for styling web pages and web applications."
+                level="Intermediate"
+                logo="https://upload.wikimedia.org/wikipedia/commons/d/d5/CSS3_logo_and_wordmark.svg"
+              />
+              <SkillCard
+                title="JavaScript"
+                description="Proficient in JavaScript programming language for web development."
+                level="Intermediate"
+                logo="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png"
               />
               <SkillCard
                 title="C/C++"
@@ -72,12 +102,6 @@ const Skills = () => {
                 logo="https://developer.apple.com/assets/elements/icons/swiftui/swiftui-96x96_2x.png"
               />
               <SkillCard
-                title="JavaScript"
-                description="Proficient in JavaScript programming language for web development."
-                level="Intermediate"
-                logo="https://upload.wikimedia.org/wikipedia/commons/6/6a/JavaScript-logo.png" // Replace with actual logo link
-              />
-              <SkillCard
                 title="SQL"
                 description="Proficient in SQL for database management and querying."
                 level="Intermediate"
@@ -87,42 +111,41 @@ const Skills = () => {
                 title="React JS"
                 description="Proficient in building user interfaces for web applications using React.js library."
                 level="Intermediate"
-                logo="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg" // Replace with actual logo link
+                logo="https://upload.wikimedia.org/wikipedia/commons/a/a7/React-icon.svg"
               />
               <SkillCard
                 title="Node.js"
                 description="Proficient in building server-side applications using Node.js runtime environment."
                 level="Intermediate"
-                logo="https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg" // Replace with actual logo link
+                logo="https://upload.wikimedia.org/wikipedia/commons/d/d9/Node.js_logo.svg" 
               />
               <SkillCard
                 title="MATLAB"
                 description="Experienced in MATLAB for numerical computing and data analysis."
                 level="Intermediate"
-                logo="https://upload.wikimedia.org/wikipedia/commons/2/21/Matlab_Logo.png" // Replace with actual logo link
+                logo="https://upload.wikimedia.org/wikipedia/commons/2/21/Matlab_Logo.png"
               />
               <SkillCard
                 title="Git"
                 description="Proficient in version control using Git."
                 level="Intermediate"
-                logo="https://upload.wikimedia.org/wikipedia/commons/3/3f/Git_icon.svg" // Replace with actual logo link
+                logo="https://upload.wikimedia.org/wikipedia/commons/3/3f/Git_icon.svg"
               />
               <SkillCard
                 title="x86-64 Assembly"
                 description="Skilled in x86-64 Assembly language programming, with a strong understanding of low-level hardware manipulation, instruction set architecture, and performance optimization techniques"
                 level="Advanced"
-                logo="https://whiteout2.gallerycdn.vsassets.io/extensions/whiteout2/x86/0.4.2/1639429284758/Microsoft.VisualStudio.Services.Icons.Default" // Replace with actual logo link
+                logo="https://whiteout2.gallerycdn.vsassets.io/extensions/whiteout2/x86/0.4.2/1639429284758/Microsoft.VisualStudio.Services.Icons.Default"
               />
               <SkillCard
                 title="Flask"
                 description="Proficient in Flask for web development and deployment. Developed the backend for the wAIste App (an app I built), managing API endpoints and integrating AI/ML models. Experienced in server configuration and optimizing application performance."
                 level="Intermediate"
-                logo={getImageUrl('Flask_Logo.png')}// Replace with actual logo link
+                logo={getImageUrl('Flask_Logo.png')}
               />
               <SkillCard
                 title="LXC Networking"
                 description="Proficient in networking concepts and configuring containers and virtual machines, with hands-on experience building containers for Honeypot projects. Designed a Honeypot Research Project to explore cybersecurity threats and vulnerabilities, investigating how attackers’ behaviors and interactions vary when encountering a high-interaction honeypot with rotating US bank names.
-                
                 https://shorturl.at/aXC5M"
                 level="Advanced"
                 logo="https://upload.wikimedia.org/wikipedia/commons/thumb/d/dd/Linux_Containers_logo.svg/1200px-Linux_Containers_logo.svg.png" 
