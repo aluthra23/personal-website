d8717359fb30fa3558c2666b7da5142e7f8d9ec0
Arav Luthra <aravluthra@Aravs-MacBook-Air.local>
2024-06-28 17:34:00 -0400

    updated linkcards with proptype

--- src/components/projects/LinkCard.jsx
+++ src/components/projects/LinkCard.jsx
@@ -1,8 +1,16 @@
 // src/components/LinkCard.jsx
 import React from 'react';
 import './LinkCard.css';
+import PropTypes from 'prop-types';
 
 const LinkCard = ({ text, url, icon, hoverColor }) => {
+  // Add 'text' to props validation
+  LinkCard.propTypes = {
+    text: PropTypes.string.isRequired,
+    url: PropTypes.string.isRequired,
+    icon: PropTypes.string.isRequired,
+    hoverColor: PropTypes.string.isRequired,
+  };
   return (
     <a href={url} target="_blank" rel="noopener noreferrer" className="link-card" style={{ '--hover-color': hoverColor }}>
       <div className="link-card-content">
