45221296fbeac39fce48343285edbf3c3b79b340
Arav Luthra <aravluthra@aravs-air.lan>
2024-07-21 04:21:31 -0400

    conciser text

--- src/components/experience/my_work/HeadstarterAI.jsx
+++ src/components/experience/my_work/HeadstarterAI.jsx
@@ -20,7 +20,7 @@ const HeadstarterAI = () => {
 
       <p className="project-description">Over the next 7 weeks (until September 9), I will develop 5 AI Projects, participate in 5 weekend hackathons, and complete a final project to get 1000+ users. Furthermore, I will receive interview prep, resume reviews, and feedback from real software engineers.</p>
       <LinkCard
-        text="Headstarter AI Fellowship Program"
+        text="Headstarter AI Fellowship"
         url="https://headstarter.co/info"
         icon="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQzkUdlMvs2RawuH6V3YyKb9mrPFaPGbHqO3Q&s"
         hoverColor="#53B4B4"
