6aa75680b2462497f13a89e9205b294cfa246ab3
Arav Luthra <aravluthra@gmail.com>
2024-08-12 18:55:49 -0400

    added youtube channel link to projects page

--- src/components/projects/Projects.jsx
+++ src/components/projects/Projects.jsx
@@ -27,13 +27,20 @@ const Projects = () => {
         <div className="projects-content">
           <h2 className="projects-title">Projects</h2>
           <div className="github-link">
-            {/* <a href="https://github.com/aluthra23" className='project-link' target="_blank">Visit my GitHub Profile</a> */}
             <LinkCard
               text='Visit my GitHub Profile'
               url="https://github.com/aluthra23"
               icon="https://www.svgrepo.com/show/475654/github-color.svg"
               hoverColor="#444"
             />
+
+            <LinkCard
+              text="Youtube Channel" 
+              url="https://youtube.com/@aravluthra6548?si=I9BlsAjwuALsVpOd" 
+              icon="https://cdn1.iconfinder.com/data/icons/logotypes/32/youtube-512.png"
+              hoverColor="#ff6666"
+            />
+
           </div>
 
           <UpcomingProjects />
