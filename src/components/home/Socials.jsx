f4116e0225853521eec34405f1806cd7c89760a5
Arav Luthra <aravluthra@gmail.com>
2024-09-11 15:01:39 -0400

    updated resume

--- src/components/home/Socials.jsx
+++ src/components/home/Socials.jsx
@@ -31,7 +31,7 @@ const Socials = () => {
         />
         <SocialCard
           icon={getImageUrl("resume.png")}
-          link="https://drive.google.com/file/d/1y7A9sVFRU2l5zi5ltddnxUaEkAgvRobT/view?usp=sharing"
+          link="https://drive.google.com/file/d/1I3HXTPAselcdKz0T26riHZG4sQAbbOCU/view?usp=sharing"
           name="Resume"
         />
         {/* Add other social media platforms */}
