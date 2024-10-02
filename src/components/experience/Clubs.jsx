6e8dfa30a01c9e8f48072f84cb0ab6c09dbc2fbe
Arav Luthra <aravluthra@gmail.com>
2024-10-02 00:24:29 -0400

    made text bigger for experience

--- src/components/experience/Clubs.jsx
+++ src/components/experience/Clubs.jsx
@@ -7,7 +7,7 @@ import AppDev from './clubs/AppDev';
 const Clubs = () => {
   return (
     <div className="computer-science-skills">
-        <h3 className='skills-subtitle' style={{'textAlign': 'center'}}>Clubs</h3>
+        <h3 className='experience-subtitle' style={{'textAlign': 'center'}}>Clubs</h3>
         <AIMLClub />
         <AppDev />
     </div>
--- src/components/experience/Work.jsx
+++ src/components/experience/Work.jsx
@@ -5,7 +5,7 @@ import TKDesign from './work/TKDesign';
 const Work = () => {
   return (
     <div className="computer-science-skills">
-        <h3 className='skills-subtitle' style={{'textAlign': 'center'}}>Internships</h3>
+        <h3 className='experience-subtitle' style={{'textAlign': 'center'}}>Internships</h3>
         <TruVoice />
         <TKDesign />
     </div>
